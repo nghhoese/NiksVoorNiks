20408c3e37a75c165242a99030f271c9e983996a
ferrannl <fhendrik1@avans.nl>
2020-02-12 12:56:24 +0100

    Header and footer
    In progress...

---
+++ PHP/header_footer.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Niks voor Niks</title>
+    <link rel="stylesheet" href="../CSS/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="../JS/script.js"></script>
+</head>
+
+<body>
+<div class="container">
+    <header class="header">
+
+            <div class="header-start">
+                <div class="logo"><a href="index.html"><img src="../Resources/favicon.png"></a></div>
+            </div>
+
+        <nav class="nav">
+
+        <div class="header-center">
+                <a href="index.html">Over ons</a>
+                <a href="index.html">Advertenties</a>
+                <a href="index.html">Activiteiten</a>
+                <a href="index.html">Deelnemer worden</a>
+                <a href="index.html">Contact</a>
+            </div>
+
+            <div class="header-end">
+                <a href="index.html"><span style="font-size: small">A</span><span style="font-size: large">A</span></a>
+                <a href="index.html" class="fa fa-bell"></a>
+                <a href="index.html">Inloggen</a>
+            </div>
+
+        </nav>
+    </header>
+
+    <div class="content">
+
+    </div>
+
+    <div class="footer">
+        <div>
+            CONTACT:
+            info@email.nl
+            +316123456789
+        </div>
+        <div>
+            &#169 2020 Niks voor Niks
+        </div>
+    </div>
+
+</div>
+</body>
+
+</html>
