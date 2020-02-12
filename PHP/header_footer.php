76e1e6c0553cdbcd205c1b1e8c41f345e644064f
ferrannl <fhendrik1@avans.nl>
2020-02-12 17:31:06 +0100

    zucht.. css is echt niet m'n ding. Morgen weer verder.

--- PHP/header_footer.php
+++ PHP/header_footer.php
@@ -4,8 +4,9 @@
 <head>
     <meta charset="utf-8">
     <title>Niks voor Niks</title>
-    <link rel="stylesheet" href="../CSS/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../CSS/style.css">
+
     <script src="../JS/script.js"></script>
 </head>
 
@@ -18,7 +19,6 @@
             </div>
 
         <nav class="nav">
-
         <div class="header-center">
                 <a href="index.html">Over ons</a>
                 <a href="index.html">Advertenties</a>
@@ -26,13 +26,11 @@
                 <a href="index.html">Deelnemer worden</a>
                 <a href="index.html">Contact</a>
             </div>
-
             <div class="header-end">
                 <a href="index.html"><span style="font-size: small">A</span><span style="font-size: large">A</span></a>
                 <a href="index.html" class="fa fa-bell"></a>
                 <a href="index.html">Inloggen</a>
             </div>
-
         </nav>
     </header>
 
