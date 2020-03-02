7413539e827210946a0eae1dbe8e4d13b19b696f
nick <nghhoese@avans.nl>
2020-03-02 11:31:47 +0100

    Header footer
    d

---
+++ footer.php
@@ -0,0 +1,13 @@
+<div class="footer">
+ <script src="JS/script.js"></script>
+<div id="innerfooter">
+    <div class="contact">
+    <p>    CONTACT:</p>
+    <p>    info@email.nl</p>
+     <p>   +316123456789</p>
+    </div>
+    <div class="copyright">
+      <p>  &#169 2020 Niks voor Niks</p>
+    </div>
+</div>
+</div>
---
+++ header.php
@@ -0,0 +1,26 @@
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<header class="header">
+
+  	 <div class="header-start">
+                <div class="logo"><a href="index.php"><img src="Resources/favicon.png"></a></div>
+            </div>
+
+            <nav class="nav">
+
+                <a href="over.php">Over ons</a>
+                <a href="advertenties.php">Advertenties</a>
+                <a href="activiteiten.php">Activiteiten</a>
+                <a href="deelname.php">Deelnemer worden</a>
+                <a href="contact.php">Contact</a>
+
+            </nav>
+
+            <div class="header-end">
+                <a href="#"><span style="font-size: small">A</span><span style="font-size: large">A</span></a>
+                <a href="#" class="fa fa-bell"></a>
+                <a href="login.php">Inloggen</a>
+            </div>
+	<button id="menu">Menu</button>
+</header>
