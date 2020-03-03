77edc72387bbafaa9fce20121d20e5de5e780d20
RickMartens <mogarian1999@gmail.com>
2020-03-03 15:30:04 +0100

    Small commit to test
    Just a small commit setting up the advertentiedetail page and testing some things

---
+++ advertentiedetails.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Niks voor Niks - Advertenties</title>
+    <link rel="icon" href="../Resources/favicon.png">
+    <link rel="stylesheet" type="text/css" href="../NiksVoorNiks/CSS/advertentiedetails.css">
+    <link rel="stylesheet" type="text/css" href="../NiksVoorNiks/CSS/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="../NiksVoorNiks/JS/script.js"></script>
+</head>
+
+<body>
+<div class="container">
+
+    <?php include("header.php"); ?>
+
+    <div class="content">
+        <h2>De Advertentiespagina</h2>
+        <div class="flex-container">
+            <div>Maincontent</div>
+            <div>Info</div>
+        </div>
+    </div>
+
+    <?php include("footer.php"); ?>
+
+</div>
+</body>
+
+</html>
