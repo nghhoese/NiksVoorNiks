35b61d565398d71736a32a19df4db8b49d812b9f
RickMartens <mogarian1999@gmail.com>
2020-03-03 16:59:08 +0100

    Start advertentiedetails
    Advertentiedetails heeft nu een foto, titel, stats en een beschrijving. Wat nog moet is een zijkant met een knop om te kopen + prijs, en een klein blokje voor de profiel

--- advertentiedetails.php
+++ advertentiedetails.php
@@ -17,10 +17,29 @@
     <?php include("header.php"); ?>
 
     <div class="content">
-        <h2>De Advertentiespagina</h2>
         <div class="flex-container">
-            <div>Maincontent</div>
-            <div>Info</div>
+            <div class="main-content">
+                <div class="title"><h1>Vers gebakken pannekoeken, taarten en brood.</h1></div>
+                <img src="../NiksVoorNiks/Resources/csm_Bakken.nl_Q2_Batch1_Pannenkoeken_Origineel_3b4e9665a9.png" >
+                <div class="stats">
+                    <div>sinds 03 maart, 2020, 15:20</div>
+                    <div>tot 05 maart, 2020, 00:00</div>
+                    <br/>
+                    <div>Koetshuis 16, Den Bosch</div>
+                </div>
+                <div class="description">
+                    <b>Beschrijving <br/></b>
+                    Koop nu snel een bakproduct bij Jantien! Ze zijn vers gebakken, smaken heerlijk en je krijgt er een gratis kopje koffie bij! Maak je geen zorgen, je mag ook lekker even blijven zitten voor de gezelligheid.<br/>
+                    <br/>
+                    Er zijn appeltaarten, kersentaarten, chocoladetaarten en peertaarten beschikbaar.<br/>
+                    <br/>
+                    Qua brood hebben we: Rogge, tarwe, gehaktbrood, krentenbollen en peperkoek.<br/>
+                    <br/>
+                    En denk eraan, alles is vers bij Jantien. Anders houdt ze het voor gezien!
+                </div>
+            </div>
+
+            <div class="info">Info</div>
         </div>
     </div>
 
