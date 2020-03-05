68f023567f3e84c509cb837a522d2ccb1b3ada7c
RickMartens <mogarian1999@gmail.com>
2020-03-05 10:01:28 +0100

    Added profile on the side
    The profile on the side contains the profile's picture, name, location and a tiny description with the option to buy the offered service

--- advertentiedetails.php
+++ advertentiedetails.php
@@ -29,17 +29,25 @@
                 </div>
                 <div class="description">
                     <b>Beschrijving <br/></b>
-                    Koop nu snel een bakproduct bij Jantien! Ze zijn vers gebakken, smaken heerlijk en je krijgt er een gratis kopje koffie bij! Maak je geen zorgen, je mag ook lekker even blijven zitten voor de gezelligheid.<br/>
+                    Koop nu snel een bakproduct bij Terry! Ze zijn vers gebakken, smaken heerlijk en je krijgt er een gratis kopje koffie bij! Maak je geen zorgen, je mag ook lekker even blijven zitten voor de gezelligheid.<br/>
                     <br/>
                     Er zijn appeltaarten, kersentaarten, chocoladetaarten en peertaarten beschikbaar.<br/>
                     <br/>
                     Qua brood hebben we: Rogge, tarwe, gehaktbrood, krentenbollen en peperkoek.<br/>
                     <br/>
-                    En denk eraan, alles is vers bij Jantien. Anders houdt ze het voor gezien!
+                    En denk eraan, alles is vers bij Terry. Niemand zegt ooit gedverderrie!
                 </div>
             </div>
 
-            <div class="info">Info</div>
+            <div class="info">
+                <img src="../NiksVoorNiks/Resources/Terry.png">
+                <div class="naam"><b>Terry Gestold</b></div>
+                <div class="locatie">Koetshuis 16, Den Bosch</div>
+                <div class="Persoonsbeschrijving">
+                    Ik ben een professioneel gitaarspeler met een hobby voor bakken en koken. In mijn vrije tijd lees ik ook graag boeken en kijk ik TV-series.
+                </div>
+                <button class="btn">Koop nu voor 5 Niks!</button>
+            </div>
         </div>
     </div>
 
