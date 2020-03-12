48f06a243bf401c452fb3e37a1d03808a1488863
Stef <sagj.vanosch@student.avans.nl>
2020-03-12 21:37:53 +0100

    Styling Homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,53 +12,55 @@ home
             <div class="slider">
                 <div class="mySlides fade">
                     <div class="numbertext">1 / 3</div>
-                    <img src="Resources/handshake.png" style="width:100%">
+                    <img src="Resources/handshake.png">
                     <div class="text">Ruilkring Niks voor Niks</div>
                 </div>
 
                 <div class="mySlides fade">
                     <div class="numbertext">2 / 3</div>
-                    <img src="Resources/denbosch.png" style="width:100%">
+                    <img src="Resources/denbosch.png">
                     <div class="text">'S Hertogenbosch en omgeving</div>
                 </div>
 
                 <div class="mySlides fade">
                     <div class="numbertext">3 / 3</div>
-                    <img src="Resources/appeltaart.png" style="width:100%">
+                    <img src="Resources/appeltaart.png">
                   <div class="text">Ruilen van diverse diensten en producten</div> 
                 
                 </div>
 
                 <!-- Next and previous buttons -->
-                <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-                <a class="next" onclick="plusSlides(1)">&#10095;</a>
+                {{-- <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                <a class="next" onclick="plusSlides(1)">&#10095;</a> --}}
             </div>
      </div>      
         
             <div class="preview-aanbod">
                
-                <div class="aanbod">
-
+                <a href="#" class="aanbod">
+                    <p class="type">Aangeboden</p>
+			        <h3 class="title">Titel advertentie</h3>                    
                     <img src="Resources/MXsfEWs.png">
-			<h1>Titel advertentie</h1>                    
+                    <p class="beschrijving">Beschrijving van de advertentie in het kort..</p>
                     <p class="price">10 Niks</p>
-                    <p>Beschrijving van de advertentie in het kort..</p>
-                    <p><button>Bekijk advertentie</button></p>
-                </div>
-                <div class="aanbod">
-                    <img src="Resources/MXsfEWs.png" alt="Aanbod">
-                    <h1>Titel advertentie</h1>
+                </a>
+                
+                <a href="#" class="aanbod">
+                    <p class="type">Aangeboden</p>
+			        <h3 class="title">Titel advertentie</h3>                    
+                    <img src="Resources/MXsfEWs.png">
+                    <p class="beschrijving">Beschrijving van de advertentie in het kort..</p>
                     <p class="price">10 Niks</p>
-                    <p>Beschrijving van de advertentie in het kort..</p>
-                    <p><button>Bekijk advertentie</button></p>
-                </div>
-                <div class="aanbod">
-                    <img src="Resources/MXsfEWs.png" alt="Aanbod">
-                    <h1>Titel advertentie</h1>
+                </a>
+                
+                <a href="#" class="aanbod">
+                    <p class="type">Aangeboden</p>
+			        <h3 class="title">Titel advertentie</h3>                    
+                    <img src="Resources/MXsfEWs.png">
+                    <p class="beschrijving">Beschrijving van de advertentie in het kort..</p>
                     <p class="price">10 Niks</p>
-                    <p>Beschrijving van de advertentie in het kort..</p>
-                    <p><button>Bekijk advertentie</button></p>
-                </div>
+                </a>
+
             </div>
      
 
