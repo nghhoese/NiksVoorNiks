50194fb9a93f275d313fcc86f963cd3fc9017c91
nick <nghhoese@avans.nl>
2020-03-17 16:13:56 +0100

    fix menu

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
-Route::get('/advertenties', function () {
-    return view('advertenties');
-});
+Route::get('/advertenties', 'Advertentie@showAdvertenties');
 Route::get('/login', function () {
     return view('login');
 });
