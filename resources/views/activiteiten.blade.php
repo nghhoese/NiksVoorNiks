3663d1a69abb49d6cdd3bbff0cbc13d117842019
Ferran <ferranhendriks@hotmail.com>
2020-04-24 22:14:50 +0200

    Activiteiten pagina...

--- resources/views/activiteiten.blade.php
+++ resources/views/activiteiten.blade.php
@@ -2,6 +2,10 @@
 @section('title')
     Activiteiten
 @endsection
+@section('stylesheets')
+    <link rel="stylesheet" href="CSS/pagination.css">
+    <link rel="stylesheet" href="CSS/activity.css">
+@endsection
 @section ('content')
     <div class="articles">
         <div class="filters">
@@ -15,12 +19,12 @@
         <div class="article-list">
             @foreach($activities as $activity)
                 <a class="article" href="/advertentieDetails/{{ $activity->id }}" id="ad1">
-                    <img src="{{$activity->foto ?? 'https://i.imgur.com/LM7EA7m.jpg'}}">
+                    <img src="{{$activity->foto ?? 'https://lh3.googleusercontent.com/proxy/57DtuXp4Ii7bGtFLht81AMAPB-y859OKGnyRf24YnXuaJahdHgjOC_zIwFjL6TXm2LMuvswnD6iL_1WtkPtNkMaIMDj5aR2mv83Md92RLEejIqHZj-JAta9ncWZoiBrZhViqB6Xhy29CW1zVGt5KhNSFPkLgd-WMBQ4pj6uqAje-U8OGtPz_EOKKSWT3MeJ_1I468cSylWSH-_2iUchESkVknSe5V_p2FStTdYfoNR8olzKDrVg6KES42ckyHiev'}}">
                     <div class="addetails">
-                        <h3 class="adtitle">{{ $activity->titel }}</h3>
+                        <h3 class="adtitle">{{ $activity->naam }}</h3>
                         <p class="addescr">{{ $activity->beschrijving }}</p>
-{{--                        <i class="fa fa-map-marker adloc"><label> Rosmalen</label></i>--}}
-                        <label class="adprice" for="ad1">{{ $activity->prijs}} Niks</label>
+                        {{--                        <i class="fa fa-map-marker adloc"><label> Rosmalen</label></i>--}}
+                        <label class="adprice" for="ad1">{{ $activity->datum}}</label>
                     </div>
                 </a>
             @endforeach
--- resources/views/advertenties.blade.php
+++ resources/views/advertenties.blade.php
@@ -1,87 +1,87 @@
 @extends ('layout')
 @section('title')
-home
+    Advertenties
 @endsection
 @section ('stylesheets')
-<link rel="stylesheet" href="CSS/pagination.css">
+    <link rel="stylesheet" href="CSS/pagination.css">
 
 @endsection
 @section ('content')
 
-<div class="articles">
-    <div class="filters">
-        <form>
-            <label class="title" for="categorie">Categorie:</label><br>
-            <select>
-                <option value="" disabled selected hidden>Kies een categorie...</option>
-                <option value="">Geen categorie</option>
-                <option value="eten">Eten</option>
-                <option value="techniek">Techniek</option>
-            </select><br>
+    <div class="articles">
+        <div class="filters">
+            <form>
+                <label class="title" for="categorie">Categorie:</label><br>
+                <select>
+                    <option value="" disabled selected hidden>Kies een categorie...</option>
+                    <option value="">Geen categorie</option>
+                    <option value="eten">Eten</option>
+                    <option value="techniek">Techniek</option>
+                </select><br>
 
 
-            <label class="title">Vraag en Aanbod:</label><br>
-            <input type="checkbox" id="gevraagd" name="gevraagd">
-            <label for="gevraagd">Gevraagd</label>
+                <label class="title">Vraag en Aanbod:</label><br>
+                <input type="checkbox" id="gevraagd" name="gevraagd">
+                <label for="gevraagd">Gevraagd</label>
 
-            <input type="checkbox" id="aangeboden" name="aangeboden">
-            <label for="aangeboden">Aangeboden</label><br>
-
-
-            <label class="title" for="locatie">Locatie:</label><br>
-            <input type="text" id="locatie" name="locatie" placeholder="Typ hier een plaats of postcode..."><br>
+                <input type="checkbox" id="aangeboden" name="aangeboden">
+                <label for="aangeboden">Aangeboden</label><br>
 
 
-            <label class="title">Prijs:</label><br>
-            <label for="minprijs">Min.</label>
-            <input type="number" id="minprijs" name="minprijs">
-
-            <label for="maxprijs">Max.</label>
-            <input type="number" id="maxprijs" name="maxprijs"><br>
+                <label class="title" for="locatie">Locatie:</label><br>
+                <input type="text" id="locatie" name="locatie" placeholder="Typ hier een plaats of postcode..."><br>
 
 
-            <label class="title" for="groep">Groep:</label><br>
-            <select>
-                <option value="" disabled selected hidden>Kies een groep...</option>
-                <option value="">Geen groep</option>
-                <option value="boxtel">Boxtel</option>
-                <option value="rosmalen">Rosmalen</option>
-                <option value="centrum">'s-Hertogenbosch Centrum</option>
-            </select><br>
+                <label class="title">Prijs:</label><br>
+                <label for="minprijs">Min.</label>
+                <input type="number" id="minprijs" name="minprijs">
 
-        </form>
+                <label for="maxprijs">Max.</label>
+                <input type="number" id="maxprijs" name="maxprijs"><br>
 
-        <a class="addad" href="advertentiePlaatsen">
-            Klik hier om zelf een advertentie te plaatsen
-            <i class="fa fa-arrow-right"></i>
-        </a>
+
+                <label class="title" for="groep">Groep:</label><br>
+                <select>
+                    <option value="" disabled selected hidden>Kies een groep...</option>
+                    <option value="">Geen groep</option>
+                    <option value="boxtel">Boxtel</option>
+                    <option value="rosmalen">Rosmalen</option>
+                    <option value="centrum">'s-Hertogenbosch Centrum</option>
+                </select><br>
+
+            </form>
+
+            <a class="addad" href="advertentiePlaatsen">
+                Klik hier om zelf een advertentie te plaatsen
+                <i class="fa fa-arrow-right"></i>
+            </a>
+        </div>
+
+        <div class="article-list">
+            @foreach($advertenties as $advertentie)
+                <a class="article" href="/advertentieDetails/{{ $advertentie->id }}" id="ad1">
+                    <img src="{{$advertentie->foto ?? 'https://i.imgur.com/LM7EA7m.jpg'}}">
+                    <div class="addetails">
+                        <p class="adtype">
+
+
+                            @if($advertentie->vraag == 0)
+                                Aangeboden
+                            @else
+                                Gevraagd
+                            @endif
+
+                        </p><br>
+                        <h3 class="adtitle">{{ $advertentie->titel }}</h3>
+                        <p class="addescr">{{ $advertentie->beschrijving }}</p>
+                        <i class="fa fa-map-marker adloc"><label> Rosmalen</label></i>
+                        <label class="adprice" for="ad1">{{ $advertentie->prijs}} Niks</label>
+                    </div>
+                </a>
+            @endforeach
+            {{$advertenties->links("pagination::bootstrap-4")}}
+        </div>
     </div>
-
-    <div class="article-list">
-    @foreach($advertenties as $advertentie)
-        <a class="article" href="/advertentieDetails/{{ $advertentie->id }}" id="ad1">
-            <img src="{{$advertentie->foto ?? 'https://i.imgur.com/LM7EA7m.jpg'}}">
-            <div class="addetails">
-                <p class="adtype">
-
-
-                    @if($advertentie->vraag == 0)
-                     Aangeboden
-                     @else
-                    Gevraagd
-                        @endif
-
-</p><br>
-                <h3 class="adtitle">{{ $advertentie->titel }}</h3>
-                <p class="addescr">{{ $advertentie->beschrijving }}</p>
-                <i class="fa fa-map-marker adloc"><label> Rosmalen</label></i>
-                <label class="adprice" for="ad1">{{ $advertentie->prijs}} Niks</label>
-            </div>
-        </a>
-@endforeach
-{{$advertenties->links("pagination::bootstrap-4")}}
-    </div>
-</div>
 
 
 
