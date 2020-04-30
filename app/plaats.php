69a4356d92204515b053f5a3ba039717e0573ee6
Ferran <ferranhendriks@hotmail.com>
2020-04-30 19:28:14 +0200

    plaats

--- app/plaats.php
+++ app/plaats.php
@@ -4,7 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class plaats extends Model
+class Plaats extends Model
 {
-    //
+    public function advertentie()
+    {
+        return $this->hasMany('App\Advertentie', 'plaats', 'naam');
+    }
 }
