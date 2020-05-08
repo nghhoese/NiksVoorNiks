afa9ac659ce69a862637f2972b6f4f81565c7aaa
nick <nghhoese@avans.nl>
2020-05-08 14:17:54 +0200

    fixes
    location required changed to correct field
    responsive mobile menu fixed

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,11 +1,12 @@
-<script src="/JS/pace.js"></script>
-<link rel="stylesheet" href="/CSS/loader.css">
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>@yield ('title')</title>
+<script src="/JS/pace.js"></script>
+<link rel="stylesheet" href="/CSS/loader.css">
 <div class="top">
 <header class="header">
 
