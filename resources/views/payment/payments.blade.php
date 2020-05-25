6553bb7c7f5c14809695633be14b745100963997
RickMartens <mogarian1999@gmail.com>
2020-05-25 11:55:19 +0200

    Small CSS fixes

--- resources/views/payment/payments.blade.php
+++ resources/views/payment/payments.blade.php
@@ -11,29 +11,29 @@
 
         <h1 class="inbox-title">Transacties </h1>
         <h1 class="inbox-title">Balans: {{$user->niksen}} Niksen</h1>
-        
+
         <div class="wrapper">
-        
+
             <div class="inbox">
 
-                
+
                 <table id="table1">
 
                     <thead>
                     <tr>
                         <th>Betreft</th>
                         <th>Bedrag</th>
-                        
+
                         <th>Datum</th>
                         <th>Status</th>
                     </tr>
                     </thead>
-                    
+
                     <tbody>
-                    
+
                     @foreach($transactions as $transaction)
                         <tr>
-                            
+
                                 <td><a class="message-link" href="/transactie/{{$transaction->id}}">
                                         <p> {{$transaction->beschrijving}}</p></a></td>
                                 @if($transaction->ontvanger_email == $user->email && $transaction->betaalverzoek == 0)
@@ -66,7 +66,7 @@
                     </tbody>
                 </table>
                 @if(count($transactions) < 1)
-                    <h3 style="text-align:center;">Je hebt Nog geen transacties </h3>
+                    <h3 style="text-align:center;">Je hebt nog geen transacties.</h3>
                 @endif
                 <div class="pagination1">
 
