3c1ef500b0eb29aa2469dba2ab3c05ace866cd95
Ferran <ferranhendriks@hotmail.com>
2020-05-25 14:54:20 +0200

    Eindelijk een fatsoenlijke footer

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -55,19 +55,15 @@
 <div class="container">
     @yield ('content')
 </div>
-<footer>
     <div class="footer">
         <script src="/JS/nav.js"></script>
         <Script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         @yield ('footer')
-        <div id="innerfooter">
             <div class="contact">
                 <p>Contact: info.letsdb@gmail.com</p>
                 <p>06-22899114</p>
                 <p>&#169 2020 Niks voor Niks</p>
             </div>
-        </div>
     </div>
-</footer>
 </body>
 </html>
